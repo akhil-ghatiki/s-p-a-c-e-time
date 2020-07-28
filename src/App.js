2f8de1de2692ccfdf263eae0396b5ff4d426f11b
Akhil Ghatiki <akhil.ghatiki@gmail.com>
2020-07-28 17:08:21 +0530

    TARS transmit initial quantum data

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import logo from './spacetime.svg';
+import './App.css';
+
+function App() {
+  return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+
+          <p>
+            Keep waiting on this.A little wiser ! A little older, but you will be happy to see this soon !
+            <br></br>
+            <p>Do not go gentle into that good night,<br></br>
+              Old age should burn and rave at close of day;<br></br>
+              Rage, rage against the dying of the light.<br></br><br></br>
+              Though wise men at their end know dark is right,<br></br>
+              Because their words had forked no lightning they<br></br>
+              Do not go gentle into that good night.<br></br><br></br>
+              Rage, rage against the dying of the light.<br></br>
+              (poem by Dylan Thomas)
+            </p>
+            -- 	&nbsp; s  	&nbsp;  	&nbsp; p   	&nbsp; 	&nbsp; a  	&nbsp;  	&nbsp;c  	&nbsp; e 	&nbsp; t 	&nbsp; i 	&nbsp;me
+          </p>
+        </header>
+      </div>
+  );
+}
+
+export default App;
