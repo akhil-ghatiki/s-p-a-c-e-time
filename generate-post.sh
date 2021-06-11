e978c76d1cc3f1c6b272082c4a751547c6ffe317
Akhil Ghatiki <akhil.ghatiki@gmail.com>
2021-06-12 01:04:23 +0530

    WIP replace route in router from generate script

--- generate-post.sh
+++ generate-post.sh
@@ -3,9 +3,27 @@
 # arguments doc
 # 1 - folder_name
 # 2 - post_name
+# b=$a:u lower case to upper case
 
-mkdir src/posts/$1
-mkdir public/images/$1
+FOLDER_NAME=$1
+FILE_NAME=$2
+FILE_NAME_UPPER_CASE=$1
 
-touch src/posts/$1/$2.md
-touch public/images/$1/$2.jpg
+route="<Route \
+path={RoutesList.$FILE_NAME_UPPER_CASE} \
+render={() => <App key="\"$FILE_NAME\"" \
+contentFilePath={$FILE_NAME} gitIssue={13} \/>} \/> \
+\{\/\*ROUTE_RENDER_PLACE_HOLDER\*\/\}"          
+
+import="import $FILE_NAME from '../posts/$FOLDER_NAME/$FILE_NAME.md';"
+
+
+routePath="$FILE_NAME_UPPER_CASE: '/$FILE_NAME'"
+
+mkdir src/posts/$FOLDER_NAME
+mkdir public/images/$FOLDER_NAME
+
+touch src/posts/$FOLDER_NAME/$FILE_NAME.md
+touch public/images/$FOLDER_NAME/$FILE_NAME.jpg
+
+sed -i '' "s/{\/\*ROUTE_RENDER_PLACE_HOLDER\*\/}/$route/" ./src/router/Routes.js
